74b823804f1c0b732e392a768e9b83876ba27c64
Sherif El Mahdi <selmahdi@hotmail.com>
2017-09-28 21:35:05 +0200

    .gitignore is added

---
+++ Module 2 - Let’s create some cool message activities/TextFormat&Attachments/HelloWorld/Controllers/MessagesController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Microsoft.Bot.Connector;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace HelloWorld
+{
+    [BotAuthentication]
+    public class MessagesController : ApiController
+    {
+        /// <summary>
+        /// POST: api/Messages
+        /// Receive a message from a user and reply to it
+        /// </summary>
+        public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
+        {
+            if (activity.Type == ActivityTypes.Message)
+            {
+                ConnectorClient connector = new ConnectorClient(new Uri(activity.ServiceUrl));
+
+                /*Using Markdown Text Format*/
+
+                //activity.TextFormat = "markdown"; 
+                //activity.Text= "My twitter account: [Sherif El Mahdi](https://twitter.com/_SherifElMahdi)";
+                //Activity reply = activity.CreateReply(activity.Text);
+
+                /*Using XML Text Format*/
+
+                //activity.TextFormat = "xml";
+                //activity.Text = "<b> Hi Mr Bot </b>";
+                //Activity reply = activity.CreateReply(activity.Text);
+
+                /*Replying with an image as an attachment*/
+
+                Activity reply = activity.CreateReply("replying with an attachemnt from the backscenes");
+                reply.Attachments = new List<Attachment>();
+                reply.Attachments.Add(new Attachment()
+                {
+                    ContentUrl = "https://botsherifhzgb2k.blob.core.windows.net/images/demopic.JPG",
+                    ContentType = "image/jpg",
+                    Name = "fromthebkscenes.jpg"
+                });
+
+                await connector.Conversations.ReplyToActivityAsync(reply);
+
+            }
+            else
+            {
+                HandleSystemMessage(activity);
+            }
+            var response = Request.CreateResponse(HttpStatusCode.OK);
+            return response;
+        }
+
+        private Activity HandleSystemMessage(Activity message)
+        {
+            if (message.Type == ActivityTypes.DeleteUserData)
+            {
+                // Implement user deletion here
+                // If we handle user deletion, return a real message
+            }
+            else if (message.Type == ActivityTypes.ConversationUpdate)
+            {
+                // Handle conversation state changes, like members being added and removed
+                // Use Activity.MembersAdded and Activity.MembersRemoved and Activity.Action for info
+                // Not available in all channels
+            }
+            else if (message.Type == ActivityTypes.ContactRelationUpdate)
+            {
+                // Handle add/remove from contact lists
+                // Activity.From + Activity.Action represent what happened
+            }
+            else if (message.Type == ActivityTypes.Typing)
+            {
+                // Handle knowing tha the user is typing
+            }
+            else if (message.Type == ActivityTypes.Ping)
+            {
+            }
+
+            return null;
+        }
+    }
+}
