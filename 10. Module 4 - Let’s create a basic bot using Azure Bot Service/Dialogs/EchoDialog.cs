387794b7a9e2923ef7eb24af6ef00cfb4548ed02
Sherif El Mahdi <selmahdi@hotmail.com>
2017-09-29 16:21:14 +0200

    Added source code of "Let’s create a basic bot using Azure Bot Service"
    Module #4 | Demo Name: Let’s create a basic bot using Azure Bot Service

---
+++ 10. Module 4 - Let’s create a basic bot using Azure Bot Service/Dialogs/EchoDialog.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Threading.Tasks;
+
+using Microsoft.Bot.Connector;
+using Microsoft.Bot.Builder.Dialogs;
+using System.Net.Http;
+
+
+namespace Microsoft.Bot.Sample.SimpleEchoBot
+{
+    [Serializable]
+    public class EchoDialog : IDialog<object>
+    {
+        public async Task StartAsync(IDialogContext context)
+        {
+            context.Wait(MessageReceivedAsync);
+        }
+
+        public async Task MessageReceivedAsync(IDialogContext context, IAwaitable<IMessageActivity> argument)
+        {
+            var message = await argument;
+            await context.PostAsync("You said: " + message.Text);
+            context.Wait(MessageReceivedAsync);
+        }
+    }
+}
---
+++ 10. Module 4 - Let’s create a basic bot using Azure Bot Service/publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=basicbotsecond-Web-Deploy.pubxml -p:Password=J2seTqnjmdzprNeev9GyQXuBaRDRGEtw2EqLAdJsvomHmDlBokm5qSPPaDak
+
