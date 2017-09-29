32262a0c8ff8edc35d93e8fbab0b2d13ca0ba9b9
Sherif El Mahdi <selmahdi@hotmail.com>
2017-09-29 08:01:13 +0200

    Added source code of FormFlow Demos
    Module #2 | Demos: "Let’s create our first FormFlow-generated dialog" and "Let’s fiddle with FormFlow attributes to customize forms"

---
+++ 06.Module 2 - FormFlow/FormFlowHello/Controllers/MessagesController.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Microsoft.Bot.Connector;
+using Newtonsoft.Json;
+using Microsoft.Bot.Builder.Dialogs;
+using static FormFlow.Forms.PickYourTShirtForm;
+using Microsoft.Bot.Builder.FormFlow;
+
+namespace FormFlowHello
+{
+    [BotAuthentication]
+    public class MessagesController : ApiController
+    {
+        /// <summary>
+        /// POST: api/Messages
+        /// Receive a message from a user and reply to it
+        /// </summary>
+        /// 
+
+        internal static IDialog<PickTShirt> MakeRootDialog()
+        {
+            return Chain.From(() => FormDialog.FromForm(PickTShirt.BuildForm)).Do(async (context, order) =>
+            {
+                try
+                {
+                    var completed = await order;
+
+                    await context.PostAsync("Just took your order");
+                }
+                catch (FormCanceledException<PickTShirt> e)
+                {
+                    string reply;
+
+                    if(e.InnerException == null)
+                    {
+                        reply = $"you quit on {e.Last}";
+                    }
+                    else
+                    {
+                        reply = "Error, please try again";
+                    }
+
+                    await context.PostAsync(reply);
+                }
+              
+            });
+        }
+
+        public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
+        {
+            if (activity.Type == ActivityTypes.Message)
+            {
+                ConnectorClient connector = new ConnectorClient(new Uri(activity.ServiceUrl));
+
+                await Conversation.SendAsync(activity, MakeRootDialog);
+
+                
+            }
+            else
+            {
+                HandleSystemMessage(activity);
+            }
+            var response = Request.CreateResponse(HttpStatusCode.OK);
+            return response;
+        }
+
+        private Activity HandleSystemMessage(Activity message)
+        {
+            if (message.Type == ActivityTypes.DeleteUserData)
+            {
+                // Implement user deletion here
+                // If we handle user deletion, return a real message
+            }
+            else if (message.Type == ActivityTypes.ConversationUpdate)
+            {
+                // Handle conversation state changes, like members being added and removed
+                // Use Activity.MembersAdded and Activity.MembersRemoved and Activity.Action for info
+                // Not available in all channels
+            }
+            else if (message.Type == ActivityTypes.ContactRelationUpdate)
+            {
+                // Handle add/remove from contact lists
+                // Activity.From + Activity.Action represent what happened
+            }
+            else if (message.Type == ActivityTypes.Typing)
+            {
+                // Handle knowing tha the user is typing
+            }
+            else if (message.Type == ActivityTypes.Ping)
+            {
+            }
+
+            return null;
+        }
+    }
+}
---
+++ 06.Module 2 - FormFlow/FormFlowHello/Forms/PickYourTShirtForm.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Bot.Builder.FormFlow;
+
+
+
+namespace FormFlow.Forms
+{
+    public class PickYourTShirtForm
+    {
+    
+        public enum SizeOptions { Small, Medium, Large };
+
+        public enum ColorOptions { Blue, Red, Black, Green, Purple };
+        
+        [Serializable]
+        public class PickTShirt
+        {
+            [Prompt ("Would you please select {&} for your t-shirt? {||}")]
+            public SizeOptions? Size;
+
+            [Optional]
+            public ColorOptions? Color;
+          
+            public static IForm<PickTShirt> BuildForm()
+            {
+                return new FormBuilder<PickTShirt>()
+                        .Message("Welcome to the simple pick your t-shirt bot!")
+                        .Build();
+            }
+        };
+    }
+}
